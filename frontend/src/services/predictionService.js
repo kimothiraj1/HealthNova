bdfe2db6b01f98b009148cf7b7e91ea5d20f6178
Ankit <ankitkimothi98@gmail.com>
2026-09-11 16:35:04 +0530

    Refactor frontend services to use VITE_API_URL environment variable

--- frontend/src/services/predictionService.js
+++ frontend/src/services/predictionService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/predict';
+const API_URL = `${import.meta.env.VITE_API_URL}/predict`;
 
 const getAuthHeader = () => {
   const token = localStorage.getItem('token');
